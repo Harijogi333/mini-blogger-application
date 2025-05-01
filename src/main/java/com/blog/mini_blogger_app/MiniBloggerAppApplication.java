b91653a10170d115a8f2f8ab056040dd5aae3703
Hari <harikumarjogi333@gmail.com>
2025-05-01 16:11:07 +0530

    intial commit

---
+++ src/main/java/com/blog/mini_blogger_app/MiniBloggerAppApplication.java
@@ -0,0 +1,13 @@
+package com.blog.mini_blogger_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBloggerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBloggerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/mini_blogger_app/MiniBloggerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.mini_blogger_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBloggerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
